ddd4ffd046cbc2580bc5dee5be184f0cb4ee5796
yamti <yam.tirosh@gmail.com>
2021-02-17 20:25:28 +0200

    Configuration values changes

--- random-tracer/sketch.js
+++ random-tracer/sketch.js
@@ -1,29 +1,30 @@
-const COLOR = {r: 255, g: 238, b: 51};
+const COLOR = {r: 255, g: 255, b: 220};
 
 let random_mover = new RandomStarlikeMover({
-	x: 400,
-	y: 400,
-	speed: 150,
+	// x and y are initialized in setup()
+	speed: 200,
 	direction: 0,
 	randomness: 0.5,
 	archness: 0.02,
 	color: COLOR,
-	changeDirectionFrameInterval: 50,
+	changeDirectionFrameInterval: 35,
 	width: 10,
 });
 
 let tracer = new Tracer2D(
 	random_mover,
 	{
-		maxTraces: 100,
+		maxTraces: 75,
 		color: COLOR,
-		headWidth: 10, 
-		widthChangeFactor: 0.6,
+		headWidth: 9, 
+		widthChangeFactor: 0.875,
 	}
 )
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	random_mover.x = windowWidth / 2;
+	random_mover.y = windowHeight / 2;
 }
 
 function draw() {
