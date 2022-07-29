a9866bc021c69b54e38b6c98f2a87a2104823035
yamti <yam.tirosh@gmail.com>
2022-07-29 16:52:53 +0300

    Write Game of Life logic

--- game-of-life/sketch.js
+++ game-of-life/sketch.js
@@ -1,3 +1,51 @@
+class GameOfLife {
+  constructor() {
+    this.world = new Set();
+  }
+
+  nextGeneration() {
+    this.world = this.#calculateNextGeneration()
+  }
+
+  #calculateNextGeneration() {
+    return new Set (
+      Array.from(this.#getPotentialAliveCells(this.world))
+      .filter(cell => this.#shouldLive(cell))
+    );
+  }
+
+  #getPotentialAliveCells() {
+    return new Set(
+        Array.from(this.world)
+        .map(cell => this.#getNeighbours(cell))
+        .flat()
+        .concat(this.world)
+      );
+  }
+
+  #shouldLive(cell) {
+    let livingNeighboursCount = this.#countLivingCells(this.#getNeighbours(cell))
+    let isCellAlive = this.#countLivingCells([cell]) === 1;
+    
+    if (livingNeighboursCount === 3) { return true; }
+    if (livingNeighboursCount === 2 && isCellAlive) { return true; }
+    return false;
+  }
+
+  #countLivingCells(cells) {
+    return cells.reduce(cell => this.world.has(cell) ? 1 : 0);
+  }
+
+  #getNeighbours(cell) {
+    return [-1, 0, 1].map(dx => {
+      [-1, 0, 1].map(dy => {
+        return {x: cell.x + dx, y: cell.y + dy};
+      })
+    }).filter(c => c != cell);  
+  } 
+}
+
+
 function setup() {
   createCanvas(400, 400);
 }
