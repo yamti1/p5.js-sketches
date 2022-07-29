caea44b110fbb1089cb14cf3e7c7f326186a7a36
yamti <yam.tirosh@gmail.com>
2022-07-29 23:11:19 +0300

    Fix game logic and check view

--- game-of-life/sketch.js
+++ game-of-life/sketch.js
@@ -1,6 +1,15 @@
 class GameOfLife {
   constructor() {
-    this.world = new Set();
+    this.world = [];
+  }
+
+  makeAlive(cell) {
+    if (this.isAlive(cell)) { return; }
+    this.world.push(cell);
+  }
+
+  isAlive(cell) {
+    return this.world.find(c => c.x === cell.x && c.y === cell.y);
   }
 
   nextGeneration() {
@@ -8,15 +17,13 @@ class GameOfLife {
   }
 
   #calculateNextGeneration() {
-    return new Set (
-      Array.from(this.#getPotentialAliveCells(this.world))
-      .filter(cell => this.#shouldLive(cell))
-    );
+    return this.#getPotentialAliveCells(this.world)
+      .filter(cell => this.#shouldLive(cell));
   }
 
   #getPotentialAliveCells() {
-    return new Set(
-        Array.from(this.world)
+    return this.#uniqueCells(
+        this.world
         .map(cell => this.#getNeighbours(cell))
         .flat()
         .concat(this.world)
@@ -42,14 +49,56 @@ class GameOfLife {
         return {x: cell.x + dx, y: cell.y + dy};
       })
     }).filter(c => c != cell);  
-  } 
+  }
+
+  #uniqueCells(cells) {
+    return cells.filter(
+      (cell, i) => i === cells.findLastIndex(
+        c => c.x === cell.x && c.y === cell.y
+      )
+    );
+  }
 }
 
 
+class GridView {
+  constructor(width, height, rows, columns) {
+    this.x = 0;
+    this.y = 0;
+    this.rows = rows;
+    this.columns = columns;
+    this.cellWidth = width / columns;
+    this.cellHeight = height / rows;
+  }
+
+  draw(game) {
+    strokeWeight(1);
+    stroke(80);
+    for (let row = 0; row < this.rows; row++) {
+      for (let column = 0; column < this.columns; column++) {
+        let isHighlighted = game.isAlive({x: this.x + column, y: this.y + row});
+        fill(isHighlighted ? 200 : 50);
+        rect(
+          column * this.cellWidth, 
+          row * this.cellHeight, 
+          this.cellWidth, 
+          this.cellHeight
+        );
+      }
+    }
+  }
+}
+
+
+let view = new GridView(400, 400, 50, 50);
+let game = new GameOfLife();
+
 function setup() {
   createCanvas(400, 400);
+  game.makeAlive({x: 10, y: 10});
 }
 
 function draw() {
   background(220);
+  view.draw(game);
 }
